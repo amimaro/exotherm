51831efd5aff9416b60c8118beab190db691a72f
amimaro <amir.zahlan@gmail.com>
2018-06-03 10:00:25 -0300

    :art: Update Home layout

--- generators/app/templates/core/src/pages/home/home.ts
+++ generators/app/templates/core/src/pages/home/home.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { ModalComponent } from '../../components/modal/modal';
+import { ModalItemComponent } from '../../components/modal-item/modal-item';
+import { PopoverComponent } from '../../components/popover/popover';
 
 import { ModalHelperProvider } from '../../providers/modal-helper/modal-helper';
+import { LoadingHelperProvider } from '../../providers/loading-helper/loading-helper';
+import { PopoverHelperProvider } from '../../providers/popover-helper/popover-helper';
+
+import { ItemProvider } from '../../providers/item/item';
+
+import { Item } from '../../models/Item';
 
 @Component({
   selector: 'page-home',
@@ -11,18 +18,44 @@ import { ModalHelperProvider } from '../../providers/modal-helper/modal-helper';
 })
 export class HomePage {
 
-  items: any = ['a', 'b'];
+  items: Item[];
 
   constructor(
     public navCtrl: NavController,
-    public modalHelper: ModalHelperProvider
+    public modalHelper: ModalHelperProvider,
+    public loadingHelper: LoadingHelperProvider,
+    public popoverHelper: PopoverHelperProvider,
+    public itemProvider: ItemProvider
   ) {
+    this.items = this.itemProvider.getItems();
   }
 
-  showModalCreate() {
-    this.modalHelper.presentModal(ModalComponent)
+  showCreateModal() {
+    this.modalHelper.presentModal(ModalItemComponent)
       .onDidDismiss(item => {
-        console.log(item)
+        if(item)
+          this.itemProvider.addItem(item);
+      });
+  }
+
+  showUpdateModal(data) {
+    this.modalHelper.presentModal(ModalItemComponent, {
+        item: data
+      })
+      .onDidDismiss(item => {
+        if(item)
+          this.itemProvider.updateItem(item);
+      });
+  }
+
+  popoverItem(item) {
+    this.popoverHelper
+      .presentPopover(PopoverComponent, {
+        options: ['Edit', 'Remove', 'Back']
+      })
+      .onDidDismiss(data => {
+        if (data === 0) this.showUpdateModal(item);
+        if (data === 1) this.itemProvider.removeItem(item);
       });
   }
 
