9e7247d5ec0b17fa12ff63880f49d71108ea0ac2
amimaro <amir.zahlan@gmail.com>
2018-06-03 09:57:44 -0300

    :sparkles Update ModalHelper

--- generators/app/templates/core/src/providers/modal-helper/modal-helper.ts
+++ generators/app/templates/core/src/providers/modal-helper/modal-helper.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ModalController } from 'ionic-angular';
 
-/*
-  Generated class for the ModalHelperProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ModalHelperProvider {
 
