e2c713279552dc97f6bb0678c506d8b172dc51c5
Amimaro <amir.zahlan@gmail.com>
2018-06-18 16:15:13 -0300

    :sparkles: Improve installation

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,10 +5,7 @@ const yosay = require('yosay');
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the bedazzling ${chalk.red('generator-exotherm')} generator!`)
-    );
+    this.log(yosay(`Welcome to ${chalk.red('exotherm')} generator!`));
 
     this.appname = this.appname.replace(/\s+/g, '-');
 
@@ -29,48 +26,51 @@ module.exports = class extends Generator {
         type: 'input',
         name: 'author',
         message: 'Author:',
-        default: this.appname
+        default: 'author'
       },
       {
         type: 'input',
         name: 'homepage',
         message: 'Homepage:',
         default: `https://${this.appname}.com`
-      },
-      {
-        type: 'confirm',
-        name: 'authentication',
-        message: 'Would you like to enable authentication?',
-        default: true
       }
     ];
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
+      this.props.destination = this.appname === this.props.name ? '.' : this.props.name;
     });
   }
 
   writing() {
+    console.log(this.props.destination);
+    const files = [
+      'src',
+      'ionic.config.json',
+      'package.json',
+      'tsconfig.json',
+      'tslint.json'
+    ];
+    for (let file of files) {
+      this.fs.copyTpl(
+        this.templatePath(`core/${file}`),
+        this.destinationPath(`${this.props.destination}/${file}`),
+        this.props
+      );
+    }
+
     this.fs.copy(
       this.templatePath('core/_.gitignore'),
-      this.destinationPath(`${this.props.name}/.gitignore`)
+      this.destinationPath(`${this.props.destination}/.gitignore`)
     );
 
     this.fs.copy(
       this.templatePath('core/.editorconfig'),
-      this.destinationPath(`${this.props.name}/.editorconfig`)
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('core'),
-      this.destinationPath(`${this.props.name}/.`),
-      this.props
+      this.destinationPath(`${this.props.destination}/.editorconfig`)
     );
   }
 
   install() {
-    process.chdir(this.props.name);
     this.npmInstall().then(() => {
       this.log('\n\nSuccessfully Done!!');
       this.log('Run ' + chalk.green('ionic serve') + ' to start.\n');
